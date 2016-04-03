667d777a1e471a1bd95ba2366454894db9ddde55
Adam Bisek <adam.bisek@gmail.com>
2016-04-03 20:57:26 +0200

    Added disabling colors via --colors 0 parameter

--- Bootstrap.php
+++ Bootstrap.php
@@ -18,6 +18,9 @@ class Bootstrap
 	public function run(InputArgs $inputArgs)
 	{
 		$workingDir = getcwd();
+		if ($inputArgs->getOption('colors') !== NULL && !$inputArgs->getOption('colors')) {
+			Cli::$enableColors = FALSE;
+		}
 		if ($inputArgs->getOption('working-dir')) {
 			$workingDir = realpath($inputArgs->getOption('working-dir'));
 			if (!$workingDir) {
