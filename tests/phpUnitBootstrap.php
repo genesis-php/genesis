ff762273d44d66ce57eb293b418dafcb98711745
Adam Bisek <adam.bisek@gmail.com>
2016-06-26 15:20:35 +0200

    Removed own Loader and used composer autoloader. Possible BC break.

--- tests/phpUnitBootstrap.php
+++ tests/phpUnitBootstrap.php
@@ -1,9 +1,3 @@
 <?php
 
-// composer loader
 require_once __DIR__ . '/../vendor/autoload.php';
-
-// genesis loader
-require_once __DIR__ . '/../src/Loader.php';
-$loader = new Genesis\Loader();
-$loader->register();
